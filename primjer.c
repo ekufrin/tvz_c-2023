baf16200148cf6f5aaa420e9e3e9caabae69af42
Kritinq <38842133+emanuelkufrin@users.noreply.github.com>
2023-11-16 18:43:52 +0100

    Primjer uploada zadatka
    Ovo je primjer kako trebate uploadati Vaše zadatke na Issues.

---
+++ primjer.c
@@ -0,0 +1,33 @@
+/*
+
+// NAPISATI TEKST ZADATKA I SVE TEST CASEOVE.
+
+TEKST ZADATKA: ......
+
+TEST CASEOVI:
+Primjer ulaza:
+1
+7 32.322 32.454 32.111 33.567 32.985 60.231 35.233
+60.231
+35.233
+Primjer izlaza:
+Vozac 1: 36.986
+Najbrzi je: vozac 1
+
+
+
+*/
+
+// ------> OVDJE ISPOD IDE VAŠ KOD
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  ......
+    ....
+      ...
+        ..
+return 0;
+}
