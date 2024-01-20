ae8fca565f84ecb4bc0201476b8434513d47e568
Kritinq <38842133+emanuelkufrin@users.noreply.github.com>
2024-01-20 17:59:07 +0100

    Create zad6.c

---
+++ kratki-ispit/2.kratki/zad6.c
@@ -0,0 +1,25 @@
+/*
+TEKST ZADATKA:
+Na 5 načina prikazati pridruživanje varijabli x koja je tipa char znamenku 7. (nadopuniti izrazom za pridruživam
+*/
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main() {
+
+	char x;
+
+	x = '7';
+	x = 0 + '7';
+	x = 'A' - 10;
+	x = 55;
+	x = '9' - 2;
+
+
+	return 0;
+}
