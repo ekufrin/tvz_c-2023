e29d27ae217b263f5d1b182041e826e03464371b
Kritinq <38842133+emanuelkufrin@users.noreply.github.com>
2024-01-20 17:04:04 +0100

    Create zad4.c

---
+++ kratki-ispit/2.kratki/zad4.c
@@ -0,0 +1,21 @@
+/*
+TEKST ZADATKA:
+Datoteka utakmice.txt sadrži podatke o utakmicama, te postoji fscanf naredba koja učitava jedan redak iz te datoteke: 
+fscanf (fin, "%[^-]%*c%[^0-9]%d : %d%*[ \n]", ut.domaci, ut.gost, &ut.domaciBrGol, &ut.gostBrGol);
+Napišite primjer dva retka datoteke koji odgovaraju gornjoj naredbi!
+*/
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+   
+	//Hrvatska-Portugal5 : 1
+	//Italija-Njemacka1 : 1
+
+
+	return 0;
+}
