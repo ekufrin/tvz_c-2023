24bb528bdd69824bf4d75b9231415c031b130beb
Kritinq <38842133+emanuelkufrin@users.noreply.github.com>
2024-01-20 21:47:37 +0100

    Update zad15.c

--- kratki-ispit/2.kratki/zad15.c
+++ kratki-ispit/2.kratki/zad15.c
@@ -24,10 +24,10 @@ typedef struct {
 
 int main() {
 	
-	tocka a, b;
+	tocka a, b;	//kreiramo varijable a i b 
 	double d;
-	scanf("%d %d %d %d", &a.x, &a.y, &b.x, &b.y);
-	d = (double)sqrt(pow((a.x - b.x), 2) + pow((a.y - b.y), 2));
+	scanf("%d %d %d %d", &a.x, &a.y, &b.x, &b.y);	//skeniramo x i y kordinate točke a i točke b
+	d = (double)sqrt(pow((a.x - b.x), 2) + pow((a.y - b.y), 2)); // formula za udaljenost dvije točke glasi: korijen iz (a.x-b.x)^2 + (a.y-b.y)^2
 	printf("%lf", d);
 
 	return 0;
