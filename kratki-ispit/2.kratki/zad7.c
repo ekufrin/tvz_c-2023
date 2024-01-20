e6ad304db8f5095274face487347cf74a26365bd
Kritinq <38842133+emanuelkufrin@users.noreply.github.com>
2024-01-20 18:01:19 +0100

    Create zad7.c

---
+++ kratki-ispit/2.kratki/zad7.c
@@ -0,0 +1,13 @@
+/*
+TEKST ZADATKA:
+Koje od navedenih definicija polja nisu ispravne ?
+a) int a[20];
+b) float [30] x;
+c) int a[20] [2] [4], h[4];
+d) char a[]="ime";
+e) double b[0];
+
+
+ODGOVOR:
+b) e)
+*/
