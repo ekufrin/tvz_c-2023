51a0b5f78a65b503d115721457a55eaf54e9ecdc
Kritinq <38842133+emanuelkufrin@users.noreply.github.com>
2024-01-24 18:13:05 +0100

    Update zad5.c

--- kratki-ispit/2.kratki/zad5.c
+++ kratki-ispit/2.kratki/zad5.c
@@ -12,17 +12,17 @@ Koju vrijednost vraća funkcija arg?
 
 int arg(int* b, int n) {
 	int i = 0, gg = *b, * p = b++; //gg poprima vrijednost polja[2], broj 11
-								   //p uzima adresu polja[2] te ga nakon toga povećava za 1, p postaje polje[3] odnosno broj 10
+								   //p uzima adresu polja[2] te ga nakon toga povećava za 1
 	do{
-		if (*p < gg) gg = *p;	// 10 < 11 ? DA		| gg postaje 10
-	} while (++i < n && p++);	//Povećavamo i za 1, povećavamo p za 1, p postaje polje[4] , izasli smo izvan granica polja.
+		if (*p < gg) gg = *p;	// 11 < 11 ? NE		| 10 < 11 ? DA , gg postaje 10		| Polje[4] je izvan granica te je to neki random broj, npr -8123818, -8123818 < 10 ? DA, gg postaje -8123818 , izlazimo iz funkcije
+	} while (++i < n && p++);	//Povećavamo i za 1, povećavamo p za 1, p postaje polje[3] 	| Povećavamo i za 1, povećavamo p za 1, p postaje polje[4]
 
 	return gg;
 }
 int main() {
 
 	int polje[4] = {13, 12, 11, 10};
-	printf("%d", arg(&polje[2], 3)); // Ispis je 10
+	printf("%d", arg(&polje[2], 3)); // Ispis je -8123818
 
 
 	return 0;
