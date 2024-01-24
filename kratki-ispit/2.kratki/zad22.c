7aef9ee8252da4a99468c7b22749830a568802f9
Kritinq <38842133+emanuelkufrin@users.noreply.github.com>
2024-01-24 20:52:06 +0100

    Create zad22.c

---
+++ kratki-ispit/2.kratki/zad22.c
@@ -0,0 +1,10 @@
+/*
+TEKST ZADATKA:
+Koju funkciju koristimo za čitanje iz binarne datoteke? Opišite argumente koje ona mora primati kako bi uspješno učitali neku strukturu iz binarne datoteke! (redoslijed opisa argumenata nije bitan)
+
+
+
+ODGOVOR:
+Koristimo funkciju fread
+fread(buffer, velicina strukture, broj redaka u datoteci, pointer na datoteku)
+*/
