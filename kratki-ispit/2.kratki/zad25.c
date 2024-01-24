5a892dc2eaf4995d9c469e8296141999a995a4a3
Kritinq <38842133+emanuelkufrin@users.noreply.github.com>
2024-01-24 21:11:30 +0100

    Create zad25.c

---
+++ kratki-ispit/2.kratki/zad25.c
@@ -0,0 +1,11 @@
+/*
+TEKST ZADATKA:
+Datoteka proizvodi.txt sadrži podatke o proizvodima, te postoji fscanf naredba koja učitava jedan redak iz te datoteke:
+fscanf (fin, "%[^|]%*c%[^0-9]%d%*c%*[a-zA-Z ]%lf%*[\n]", p.naziv, p.kategorija, &p.kolicina, &p.cijena);
+Napišite primjer dva retka datoteke koji odgovaraju gornjoj naredbi, gdje je bitno pokazati primjer koji od učitanih vrijednosti može imati razmake u sebi!
+
+
+ODGOVOR:
+Goveđi gulaš| Gulaši5 19.99
+Kako naučiti C?| Knjiga1 34.21
+*/
